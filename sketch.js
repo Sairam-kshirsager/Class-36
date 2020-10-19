e0fd57ad20565541cff97b350d3eed24e7defee9
Sairam-kshirsager <70512559+Sairam-kshirsager@users.noreply.github.com>
2020-10-19 15:15:15 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,23 @@
+var database;
+var playerCount;
+
+var canvas,backgroundImage;
+var gameState = 0;
+var form,player,game;
+
+
+function setup(){
+ canvas = createCanvas(400,400);
+  database = firebase.database();
+  game=new Game();
+  game.getState();
+  game.start();
+  
+}
+
+function draw(){
+ 
+  
+}
+
+
